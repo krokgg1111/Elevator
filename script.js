2c04c0a9e71f4f9d69d371b09b43f1c81274b595
KrokGG1 <krokg1.me@gmail.com>
2026-03-03 19:59:25 +0200

    Commit A3

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnd = document.getElementById("btn-down");
+const btnu = document.getElementById("btn-up");
+const elv = document.getElementById("elevator");
+const currentpos = 0;
+
+btnd.addEventListener("click", function(){
+
+});
+
+btnu.addEventListener("click", function(){
+    elv.style.bottom = 80+"px";
+});
