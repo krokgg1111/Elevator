e07683d18a18c450278b140ec4218a52678d98c9
KrokGG1 <krokg1.me@gmail.com>
2026-03-10 19:09:31 +0200

    Commit B1

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const btnd = document.getElementById("btn-down");
 const btnu = document.getElementById("btn-up");
 const elv = document.getElementById("elevator");
-const currentpos = 0;
+const flr = document.getElementById("floor");
+let move = 0;
+let fl = 1;
+let sound = new Audio();
+sound.src = "bell.mp3";
+flr.textContent = fl;
 
 btnd.addEventListener("click", function(){
-
+    if (move > 0){
+        elv.style.bottom = move - 80 + "px";
+        move = move - 80;
+        fl --;
+        flr.textContent = fl;
+        sound.play();
+    }
 });
 
 btnu.addEventListener("click", function(){
-    elv.style.bottom = 80+"px";
+    if (move <= 240){
+        elv.style.bottom = move + 80 + "px";
+        move = move + 80;
+        fl ++;
+        flr.textContent = fl;
+        sound.play();
+    }
 });
